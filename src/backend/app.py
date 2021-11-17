b65ec2ac0181d472d71d1d08a781a2098bc23875
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2021-11-17 14:57:26 +0100

    Dockershit

---
+++ src/backend/app.py
@@ -0,0 +1,81 @@
+from flask import Flask, request
+import json
+from elastic_index import Index
+from store import Store
+from handlers import timbuctoo
+
+
+app = Flask(__name__)
+
+config = {
+    "url" : "elastic",
+    "port" : "9200",
+    "doc_type" : "procrustes"
+}
+
+index = Index(config)
+tb = timbuctoo.Timbuctoo_handler()
+
+
+@app.after_request
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    response.headers['Content-type'] = 'application/json'
+    return response
+
+@app.route("/")
+def hello_world():
+    retStruc = {"app": "Procrustus service", "version": "0.1"}
+    return json.dumps(retStruc)
+
+@app.route("/facet", methods=['GET'])
+def get_facet():
+    facet = request.args.get("name")
+    amount = request.args.get("amount")
+    ret_struc = index.get_facet(facet + ".keyword", amount)
+    return json.dumps(ret_struc)
+
+@app.route("/filter-facet", methods=['GET'])
+def get_filter_facet():
+    facet = request.args.get("name")
+    amount = request.args.get("amount")
+    facet_filter = request.args.get("filter")
+    ret_struc = index.get_filter_facet(facet + ".keyword", amount, facet_filter)
+    return json.dumps(ret_struc)
+
+@app.route("/browse", methods=['POST'])
+def browse():
+    struc = request.get_json()
+    ret_struc = index.browse(struc["page"], struc["page_length"], struc["sortorder"] + ".keyword", struc["searchvalues"], struc["index"])
+    return json.dumps(ret_struc)
+
+@app.route("/get_store")
+def get_store():
+    store = Store()
+    return store.get_data()
+
+@app.route("/get_entities/<ds>", methods=["GET"])
+def get_entities(ds):
+    result = tb.get_colls(ds)
+    query = {"query": result}
+    return json.dumps(result)
+
+@app.route("/get_collection_properties/<ds>/<coll>")
+def get_properties(ds, coll):
+    result = tb.get_props(ds, coll)
+    return json.dumps(result)
+
+@app.route("/get_item", methods=["POST"])
+def get_item():
+    params = request.get_json()
+    result = tb.get_item(params["dataset"], params["collection"], params["uri"])
+    return json.dumps(result)
+
+
+#Start main program
+
+if __name__ == '__main__':
+    app.run()
+
---
+++ src/backend/handlers/timbuctoo.py
@@ -0,0 +1,118 @@
+import requests
+import json
+
+from queries import timbuctoo_queries, timbuctoo_fragments
+
+class Timbuctoo_handler:
+    def __init__(self):
+        self.server = "https://repository.goldenagents.org/v5/graphql"
+        self.tq = timbuctoo_queries.Timbuctoo_queries()
+        self.tf = timbuctoo_fragments.Timbuctoo_fragments()
+
+
+    def fetch_data(self, query):
+        params = {"query": query}
+        results = requests.post(self.server, json = params, headers = {'Content-Type': 'application/json',
+                                                                       "VRE_ID": "ufab7d657a250e3461361c982ce9b38f3816e0c4b__ecartico_20200316"})
+        return json.loads(results.text)
+
+    def get_colls(self, dataset):
+        query = self.tq.get_collections(dataset)
+        return self.fetch_data(query)
+
+    def get_props(self, dataset, collection):
+        query = self.tq.get_collection_properties(dataset, collection)
+        return self.fetch_data(query)
+
+
+
+    def create_field_query(self, item):
+        if item["isList"]:
+            if len(item["referencedCollections"]["items"]) > 1:
+                return self.tf.entity_list(item["name"])
+            else:
+                if item["isValueType"]:
+                    return self.tf.default_value_list(item["name"])
+                else:
+                    return self.tf.uri_title_value_list(item["name"])
+        else:
+            if len(item["referencedCollections"]["items"]) > 1:
+                return self.tf.entity(item["name"])
+            else:
+                if item["isValueType"]:
+                    if len(item["referencedCollections"]["items"]) == 1:
+                        return self.tf.entity(item["name"])
+                    else:
+                        return self.tf.default_value(item["name"])
+                else:
+                    return self.tf.uri_title_value(item["name"])
+
+
+    def value_extractor(self, props):
+        subquery = "title {value} "
+        for item in props["data"]["dataSetMetadata"]["collection"]["properties"]["items"]:
+            subquery = subquery + self.create_field_query(item)
+        return subquery
+
+
+    def build_query(self, dataset, collection, uri):
+        props = self.get_props(dataset, collection)
+        values = self.value_extractor(props)
+        query = self.tf.get_item(dataset, collection, uri, values)
+        return query
+
+    def get_value(self, item):
+        if "uri" in item.keys():
+            return item["title"]["value"]
+        else:
+            return item["value"]
+
+    def humanify_notion(self, field):
+        ret_value = field.replace("_inverse_", "")
+        ret_value = ret_value.replace("List", "")
+        ret_value = ret_value.replace("schema_", "")
+        return ret_value
+
+    def put_list_values(self, field, data):
+        values = []
+        for item in data["items"]:
+            values.append(self.get_value(item))
+        return {"notion": field, "label": self.humanify_notion(field), "values": values}
+
+
+    def unify_data(self, field, data):
+        if data == None:
+            return {"notion": field, "label": self.humanify_notion(field), "values": []}
+        else:
+            if "items" in data.keys():
+                return self.put_list_values(field, data)
+            else:
+                values = []
+                values.append(self.get_value(data))
+                return {"notion": field, "label": self.humanify_notion(field), "values": values}
+
+
+    def model_results(self, dataset, collection, result):
+        items = []
+        for field in result["data"]["dataSets"][dataset][collection]:
+            items.append(self.unify_data(field, result["data"]["dataSets"][dataset][collection][field]))
+        return items
+
+    def rdf_label_as_title(self, title, items):
+        ret_title = title
+        for item in items:
+            if item["notion"] == "rdfs_label":
+                ret_title = item["values"][0]
+        return ret_title
+
+    def get_item(self, dataset, collection, uri):
+        query = self.build_query(dataset, collection, uri)
+        result = self.fetch_data(query)
+        items = self.model_results(dataset, collection, result)
+        tmp = items.pop(0);
+        title = tmp["values"][0]
+        title = self.rdf_label_as_title(title, items)
+        return {"title": title, "uri": uri, "items": items}
+        #return json.dumps({"query": query})
+
+
